54d44d91a569d48a6ca7c7f6676eb550a3aea2fe
NawafSwe <nawaf.jr9@hotmail.com>
2021-10-04 13:42:10 +0300

    feat: endpoint ready for frontend

--- app/controllers/api/v1/coins_controller.rb
+++ app/controllers/api/v1/coins_controller.rb
@@ -4,6 +4,19 @@ require 'net/http'
 
 class Api::V1::CoinsController < ApplicationController
     def index 
-        puts "hello world"
+        url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/info'
+        if params[:symbol] != nil
+            url += '?symbol=' + params[:symbol]
+        end 
+        uri = URI.parse(url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        # assuming http is generated like this http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        # might need this as well? 
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        request = Net::HTTP::Get.new(uri.request_uri)
+        request['X-CMC_PRO_API_KEY'] = ENV['CRYPTO_API_KEY']
+        res = http.request(request)
+        render json:res.body
     end
 end
