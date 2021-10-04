da82ecbe4f1593faba7edcf86ac75a729816d662
NawafSwe <nawaf.jr9@hotmail.com>
2021-10-04 14:44:15 +0300

    hotfix: heroku

--- app/controllers/api/v1/coins_controller.rb
+++ app/controllers/api/v1/coins_controller.rb
@@ -4,6 +4,7 @@ require 'net/http'
 
 class Api::V1::CoinsController < ApplicationController
     def index 
+        
         url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/info'
         if params[:symbol] != nil
             url += '?symbol=' + params[:symbol]
